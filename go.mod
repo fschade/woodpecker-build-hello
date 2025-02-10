78bf407cccc44254406eb3d97d4288c746140ae2
Florian Schade <f.schade@icloud.com>
2025-02-10 18:24:42 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fschade/woodpecker-hello
+
+go 1.23
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := "woodpecker"
+	fmt.Printf("Hello and welcome, %s!\n", s)
+}
